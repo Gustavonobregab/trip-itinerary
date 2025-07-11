6732ffe3b536a4eeb2d5f0f59983d2619aa0feb1
Gustavo da Nóbrega <gustavonobg@gmail.com>
2025-07-11 20:42:28 -0300

    feat: add loading on main-trips

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import LoginPage from '@/components/LoginForm';
-import '@tabler/core/dist/js/tabler.min.js';
 import '@tabler/core/dist/css/tabler.min.css';
 import '@/app/globals.css';
 
--- src/components/TripList.tsx
+++ src/components/TripList.tsx
@@ -14,10 +14,13 @@ export default function TripList() {
     const [trips, setTrips] = useState<Trip[]>([]);
     const [page, setPage] = useState(1);
     const [hasMore, setHasMore] = useState(true);
+    const [loading, setLoading] = useState(false);
     const limit = 6;
 
     
     const fetchTrips = async (currentPage: number) => {
+        setLoading(true);
+        try {
         const res = await fetch(`/api/trips?page=${currentPage}&limit=${limit}`);
         const result = await res.json();
     
@@ -27,11 +30,25 @@ export default function TripList() {
         } else {
           console.error(result.error);
         }
+    } catch (error) {
+        console.error("Failed to fetch trips:", error);
+      } finally {
+        setLoading(false);
+      }
       };
     
       useEffect(() => {
         fetchTrips(page);
       }, [page]);
+
+  if (loading && trips.length === 0) {
+    return (
+      <div className="flex justify-center items-center py-20">
+        <div className="spinner-border" role="status"></div>
+      </div>
+    );
+  }
+
     
   return (
         <div className="w-full px-3 sm:px-6 py-10 pb-20 box-border">
@@ -60,15 +77,19 @@ export default function TripList() {
       </div>
 
       {hasMore && (
-        <div className="mt-10 text-center">
-          <button
-            onClick={() => setPage((prev) => prev + 1)}
-            className="px-6 py-2 bg-black text-white rounded hover:bg-gray-800 transition"
-          >
-            Load more trips
-          </button>
-        </div>
-      )}
+            <div className="mt-10 text-center">
+              {loading ? (
+                <div className="spinner-border" role="status"></div>
+              ) : (
+                <button
+                  onClick={() => setPage((prev) => prev + 1)}
+                  className="px-6 py-2 bg-black text-white rounded hover:bg-gray-800 transition"
+                >
+                  Load more trips
+                </button>
+              )}
+            </div>
+          )}
     </div>
   );
   
