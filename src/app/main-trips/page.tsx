a52270445f54e589bf69944f11504e51dd8cf9f8
Gustavo da Nóbrega <gustavonobg@gmail.com>
2025-07-11 23:46:56 -0300

    fix: changing title to homePage

--- src/app/main-trips/page.tsx
+++ src/app/main-trips/page.tsx
@@ -6,16 +6,7 @@ export default function TripsPage() {
   return (
     <section className="px-4 py-10 lg:py-20">
       <div className="max-w-screen-xl mx-auto">
-      <h1 className="text-[10vw] font-bold text-black leading-tight">
-  Plan your inTripnerary
-</h1>
-        <p className="mt-2 max-w-xl text-lg text-gray-600">
-          From routes to timing, make every part of your journey simple, smooth, and beautifully planned.
-        </p>
-      </div>
-
-      <div className="mt-10 max-w-screen-xl mx-auto">
-        <TripList />
+           <TripList />
       </div>
     </section>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,5 +5,17 @@ import '@tabler/core/dist/css/tabler.min.css';
 import '@/app/globals.css';
 
 export default function Home() {
-  return <TripsPage />;
+  return (
+    <section className="px-4 py-10 lg:py-28">
+      <div className="max-w-screen-2xl mx-auto text-center px-4">
+        <h1 className="text-[24vw] sm:text-[8vw] font-extrabold text-black leading-tight">
+          Plan your inTripnerary
+        </h1>
+        <p className="mt-4 max-w-2xl mx-auto text-xl sm:text-2xl text-gray-600">
+          From routes to timing, make every part of your journey simple, smooth, and beautifully planned.
+        </p>
+      </div>
+      <TripsPage />
+    </section>
+  );
 }
